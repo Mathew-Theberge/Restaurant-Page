c32b83052ee2d2a202b484c6ed587fdce8082961
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-09-19 11:48:32 -0400

    Add layout and style for menu page

--- src/menu.js
+++ src/menu.js
@@ -27,6 +27,15 @@ const img6 = document.createElement("img")
 
 const imgArray = [img1, img2, img3, img4, img5, img6]
 
+const title1 = document.createElement("div")
+const title2 = document.createElement("div")
+const title3 = document.createElement("div")
+const title4 = document.createElement("div")
+const title5 = document.createElement("div")
+const title6 = document.createElement("div")
+
+const titleArray = [title1, title2, title3, title4, title5, title6]
+
 const cost1 = document.createElement("div")
 const cost2 = document.createElement("div")
 const cost3 = document.createElement("div")
@@ -51,34 +60,41 @@ export function renderMenuPage() {
     content.setAttribute("class", "menu")
 
     img1.setAttribute("alt", "burger and fries")
+    title1.textContent = "Burger & fries"
     cost1.textContent = "$13"
     calories1.textContent = "1100 calories"
 
     img2.setAttribute("alt", "pizza")
+    title2.textContent = "Pizza"
     cost2.textContent = "$15"
     calories2.textContent = "1500 calories"
 
     img3.setAttribute("alt", "steak, fries and greens")
+    title3.textContent = "Steak, fries & greens"
     cost3.textContent = "$25"
     calories3.textContent = "950 calories"
 
     img4.setAttribute("alt", "chicken wings")
+    title4.textContent = "Chicken wings"
     cost4.textContent = "$18"
     calories4.textContent = "800 calories"
 
     img5.setAttribute("alt", "sushi")
+    title5.textContent = "Sushi"
     cost5.textContent = "$20"
     calories5.textContent = "500 calories"
 
     img6.setAttribute("alt", "bowl of ramen")
+    title6.textContent = "Ramen"
     cost6.textContent = "$14"
     calories6.textContent = "750 calories"
 
     for (let i = 0; i < 6; i++) {
-        imgArray[i].setAttribute("width", "400px")
-        imgArray[i].setAttribute("height", "266px")
+        imgArray[i].setAttribute("width", "300px")
+        imgArray[i].setAttribute("height", "200px")
         imgArray[i].src = imageReferanceArray[i]
-        cardArray[i].append(imgArray[i], costArray[i], calorieArray[i])
+        cardArray[i].setAttribute("class", "card")
+        cardArray[i].append(imgArray[i], titleArray[i], costArray[i], calorieArray[i])
         content.append(cardArray[i])
     }
 
